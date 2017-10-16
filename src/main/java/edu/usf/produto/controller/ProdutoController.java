a79c977e06f288859aac58cdef2db894df34ce75
Robson Luz <robson.luz@gmail.com>
2017-10-16 16:32:55 -0200

    Metodo delete

--- src/main/java/edu/usf/produto/controller/ProdutoController.java
+++ src/main/java/edu/usf/produto/controller/ProdutoController.java
@@ -3,6 +3,10 @@ package edu.usf.produto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,4 +29,11 @@ public class ProdutoController {
 	public Produto save(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
+	
+	@RequestMapping(path="/{id}", method=RequestMethod.DELETE)
+	public HttpEntity<?> delete(@PathVariable Long id) {
+		produtoRepository.delete(id);
+		return new ResponseEntity(HttpStatus.OK);
+	}	
+	
 }
